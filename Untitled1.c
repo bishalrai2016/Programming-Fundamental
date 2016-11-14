ce9ccd8f460625a4db8bbff92af8018d557ba265
bishalrai2016 <bishal.rai2k16@gmail.com>
2016-11-14 13:09:44 +0545

    Add files via upload
    C program to print the name

---
+++ Untitled1.c
@@ -0,0 +1,19 @@
+/*
+
+Student Name:Bishal Rai
+Subject:Programming Fundamental
+Roll no:
+Lab Sheet No:1
+Program:Write a C program to print your name
+Date:14/11/2016
+
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+printf("Bishal Rai");
+getch();
+return 0;
+}
