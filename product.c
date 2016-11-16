f34e191e55098cc3684305a0f5f0fe166775f898
bishalrai2016 <bishal.rai2k16@gmail.com>
2016-11-16 10:31:04 +0545

    Add files via upload
    Write a c program to print product of two no. asked from users

---
+++ product.c
@@ -0,0 +1,26 @@
+/*
+Student Name:Bishal Rai
+Subject:Programming Fundamental
+Roll no:04
+Lab sheet no:02
+Program:WAP to print product of two no. asked fro users
+Date:16/11/2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,pro;
+printf("Enter the first number\n");
+scanf("%d",&a);
+printf("Enter the second number\n");
+scanf("%d",&b);
+
+
+   pro=(a*b);
+
+printf("The product of two no is %d",pro);
+getch();
+return(0);
+}
