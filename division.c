05361ebbb018039898b1f3303c2e140500a0f9f1
bishalrai2016 <bishal.rai2k16@gmail.com>
2016-11-16 12:25:51 +0545

    Add files via upload
    Write a C program to find the division of two numbers input by users

---
+++ division.c
@@ -0,0 +1,23 @@
+/*
+Student Name:Bishal Rai
+Subject:Programming Fundamental
+Roll no:04
+Lab sheet no:02
+Program:WAP to print the division of two numbers input by users
+Date:16/11/2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float a,b,div;
+printf("Enter the first number\n");
+scanf("%f",&a);
+printf("Enter the second number\n");
+scanf("%f",&b);
+div=(a/b);
+printf("The division of two no is %f",div);
+getch();
+return(0);
+}
+
