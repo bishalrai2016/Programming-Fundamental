248f36c1a80562b988606a4667eb100010734647
bishalrai2016 <bishal.rai2k16@gmail.com>
2016-11-18 12:55:42 +0545

    Add files via upload
    A program to calculate area and circumference of circle having radius r

---
+++ area.c
@@ -0,0 +1,27 @@
+/*
+Student Name:Bishal Rai
+Subject:Programming Fundamental
+Roll no:04
+Lab sheet no:03
+Program:A C program to calculate area and circumference of circle having radius r.r should taken from user
+Date:18/11/2016
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define PI 3.14
+int main()
+{
+    float r,area,cir;
+    printf("Enter the radius:\n");
+    scanf("%f",&r);
+    area=PI*pow(r,2);
+    cir=2*PI*r;
+    printf("----------------------------------------\n");
+    printf("The Area of circle is %f\n",area);
+    printf("----------------------------------------\n");
+    printf("The Circumference of cicle is %f\n",cir);
+    printf("*****************************************");
+    getch();
+    return(0);
+}
