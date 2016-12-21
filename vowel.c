cc8a44d518573fe0655d87f95631cca157847e62
bishalrai2016 <bishal.rai2k16@gmail.com>
2016-12-21 10:21:29 +0545

    Add files via upload
    A C program to check character assigned from user is vowel or not

---
+++ vowel.c
@@ -0,0 +1,24 @@
+/*
+Student Name:Bishal Rai
+Subject:Programming fundamental
+Roll no:11
+Lab sheet no:14
+Program:A C program to check character assigned from user is vowel or not.
+Date:21/12/2016
+*/
+#include<stdio.h>
+int main()
+{
+char a;
+printf("Enter any character\n");
+scanf("%c",&a);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+{
+	printf("%c is vowel",a);
+}
+else
+{
+	printf("%c is not vowel",a);
+}		
+return(0);
+} 
