4c8f443b98696666a32c897c08730aa808f2ee81
bishalrai2016 <bishal.rai2k16@gmail.com>
2017-01-11 10:56:25 +0545

    Add files via upload
    A Program to check whether a number is even or odd using ternary operator

---
+++ even.c
@@ -0,0 +1,17 @@
+/*
+Student Name:Bishal Rai
+Subject:Programming Fundamental
+Roll no:13
+Lab Sheet no:16
+Program:A program to check whether a numnber is even or odd using ternary operator
+Date:09/01/2017
+*/
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter any number\n");
+	scanf("%d",&a);
+	(a%2==0)?printf("The number is Even"):printf("The number is Odd");
+	return(0);
+}	 
