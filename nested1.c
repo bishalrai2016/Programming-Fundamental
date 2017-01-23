716c18d8303a8920b06306dcda71d71d48f0d2f6
bishalrai2016 <bishal.rai2k16@gmail.com>
2017-01-23 12:52:27 +0545

    Add files via upload
    A program to display output *****
                                                 ****
                                                 ***
                                                 **
                                                 *

---
+++ nested1.c
@@ -0,0 +1,28 @@
+/*
+Student Name:Bishal Rai
+subject:Programming Fundamental
+Roll No:13
+Lab Sheet no:21
+Program:To display output *****
+                          ****
+                          ***
+                          **
+                          *
+Date:20/01/2017
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=5;j>=i;j--)
+        {
+            printf("*");
+        }
+    printf("\n");
+    }
+    getch();
+    return(0);
+}
