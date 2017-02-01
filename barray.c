e5b2a0a6d9606a1bcb4cee6661cd480c8c5d1d75
bishalrai2016 <bishal.rai2k16@gmail.com>
2017-02-01 11:03:29 +0545

    Add files via upload
    WAP to declare array and assign value

---
+++ barray.c
@@ -0,0 +1,20 @@
+/*
+Student Name:Bishal Rai
+Subject:Programming fundamental
+Roll no:13
+Lab sheet no:25
+Program:To declare array and assign value
+Date=01/02/2017
+*/
+#include<stdio.h>
+int main()
+{
+	int a[5]={1,2,3,4,5};
+	int i;
+	for(i=0;i<5;i++)
+	{
+		printf("a[%d]=%d \n",i,a[i]);
+	}
+	return 0;
+}	
+		
